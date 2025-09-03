53bd0cfedcd306a139f152f6a86305e9ed611346
snehalreddy987 <162408473+snehalreddy987@users.noreply.github.com>
2025-09-03 16:34:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/versitel-website/', // Replace with your repository name
+  base: '/versitel-website1/', // Replace with your repository name
   server: {
     port: 3000,
     open: true
   }
 })
+
